66a61ee67b7e10c60a70ffb02a6475947be238e9
Sandeep Yalamarthi <sandeep.yalamarthi@oracle.com>
2022-09-20 01:19:11 +0530

    add swagger

--- src/main/java/com/sample/commons/simplebankingapp/request/CreateAccountRequest.java
+++ src/main/java/com/sample/commons/simplebankingapp/request/CreateAccountRequest.java
@@ -1,26 +1,37 @@
 package com.sample.commons.simplebankingapp.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sample.commons.simplebankingapp.model.Account;
-import lombok.Data;
-import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sample.commons.simplebankingapp.model.Account;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 @Data
 @Component
+@ApiModel
 public class CreateAccountRequest {
 
     @JsonProperty("documentCode")
+    @ApiModelProperty(notes = "Represents a document code", example = "5551245")
     private String documentCode;
 
 
     @NotBlank(message = "Name is required.")
+    @JsonProperty("name")
+    @ApiModelProperty(notes = "Represents a user name", example = "ABCD")
     private String name;
 
     @NotNull
+    @JsonProperty("phoneNumber")
+    @ApiModelProperty(notes = "Represents a user phone number", example = "+9194728453456")
     private Long phoneNumber;
 
 
--- src/main/java/com/sample/commons/simplebankingapp/request/UpdateAccountRequest.java
+++ src/main/java/com/sample/commons/simplebankingapp/request/UpdateAccountRequest.java
@@ -2,6 +2,9 @@ package com.sample.commons.simplebankingapp.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sample.commons.simplebankingapp.model.Account;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +12,15 @@ import javax.validation.constraints.Pattern;
 
 @Data
 @Component
+
+@ApiModel
 public class UpdateAccountRequest {
 
     @JsonProperty("documentCode")
+    @ApiModelProperty(notes = "Represents a document code", example = "5551245")
     private String documentCode;
 
-
+    @ApiModelProperty(notes = "Represents a user phone number", example = "+9194728453456")
     private Long phoneNumber;
 
 
